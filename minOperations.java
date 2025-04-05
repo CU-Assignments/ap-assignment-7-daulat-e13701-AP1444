bf63dbdc146985312073cd155c28e0e0d1912f0f
AP1444 <155886003+AP1444@users.noreply.github.com>
2025-04-05 13:15:25 +0530

    Create minOperations.java

---
+++ minOperations.java
@@ -0,0 +1,21 @@
+class Solution {
+    public static int minOperations(int[] nums) {
+        
+    	int count = 0;
+    	
+    	for (int i = 0; i < nums.length-1; i++) {
+			
+    		if(nums[i] >= nums[i+1]) {
+    			
+                int diff = nums[i] - nums[i + 1] + 1;
+    			
+    			count += diff;
+    			
+    			
+    			nums[i+1] += diff;
+    		}
+    		
+		}
+    	return count;
+    }
+}
